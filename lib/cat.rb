a09486b93dd1ff33672c2aa4f062198252756439
James Johnson <jtaylorjohnson@gmail.com>
2020-04-09 21:35:18 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -6,8 +6,8 @@ class Cat
   
   def initialize(name, owner)
     @name = name
-    @owner = owner
-    
+    @owner = owner #belongs to
+    owner.pets[:cats] << self #has many
     @mood = "nervous"
     @@all << self
   end
